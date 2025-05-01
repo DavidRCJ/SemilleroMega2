3ad2644e5213bd39894161fee93ce798fccb45b2
David <davidricardocruzjuarez@gmail.com>
2025-05-01 17:11:50 -0600

    Agregamos fuinciones a angular la primera en migrar es movies.HTML

---
+++ semillero-angular/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+////import { Routes } from '@angular/router';
+////export const routes: Routes = [];
+
+import { MoviesComponent } from './pages/movies/movies.component';
+export const routes: Routes = [
+  { path: 'movies', component: MoviesComponent },
+  // otras rutas ya agregadas
+];
